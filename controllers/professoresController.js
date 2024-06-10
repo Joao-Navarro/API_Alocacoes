851e59fd4dd28346f391ede95c07cb3b5b8771de
GuDonizete <gustavolimasouza466@gmail.com>
2024-06-10 13:59:31 -0300

    ontroller-pt2

--- controllers/professoresController.js
+++ controllers/professoresController.js
@@ -1,6 +1,6 @@
 //ProfessoresController.js
 const Professores = require('../models/professoresModel');
-// Controlador para obter todos os Professoress
+
 exports.getAllProfessoress = (req, res) => {
     Professores.getAllProfessoress((err, Professoress) => {
         if (err) {
@@ -8,51 +8,5 @@ exports.getAllProfessoress = (req, res) => {
         } else {
             res.json(Professoress);
         }
-    });
-};
-// Controlador para obter um Professores pelo ID
-exports.getProfessoresById = (req, res) => {
-    Professores.getProfessoresById(req.params.id, (err, Professores) => {
-        if (err) {
-            res.status(500).send(err);
-        } else if (Professores) {
-            res.json(Professores);
-        } else {
-            res.status(404).send({ message: 'Professor não encontrado' });
-        }
-    });
-};
-// Controlador para criar um novo Professores
-exports.createProfessores = (req, res) => {
-    Professores.createProfessores(req.body, (err, result) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.status(201).json(result);
-        }
-    });
-};
-// Controlador para atualizar um Professores existente
-exports.updateProfessores = (req, res) => {
-    Professores.updateProfessores(req.params.id, req.body, (err, result) => {
-        if (err) {
-            res.status(500).send(err);
-        } else if (result.changes) {
-            res.status(200).json(result);
-        } else {
-            res.status(404).send({ message: 'Professor não encontrado' });
-        }
-    });
-};
-// Controlador para deletar um Professores
-exports.deleteProfessores = (req, res) => {
-    Professores.deleteProfessores(req.params.id, (err, result) => {
-if (err) {
-        res.status(500).send(err);
-    } else if (result.changes) {
-        res.status(200).json({ message: 'Professor deletado com sucesso' });
-    } else {
-        res.status(404).send({ message: 'Professor não encontrado' });
-    }
     });
 };
--- controllers/salasController.js
+++ controllers/salasController.js
@@ -1,6 +1,5 @@
-//SalasController.js
 const Salas = require('../models/salasModel');
-// Controlador para obter todos os Salas
+
 exports.getAllSalas = (req, res) => {
     Salas.getAllSalas((err, Salas) => {
         if (err) {
@@ -8,51 +7,5 @@ exports.getAllSalas = (req, res) => {
         } else {
             res.json(Salas);
         }
-    });
-};
-// Controlador para obter um Salas pelo ID
-exports.getSalasById = (req, res) => {
-    Salas.getSalasById(req.params.id, (err, Salas) => {
-        if (err) {
-            res.status(500).send(err);
-        } else if (Salas) {
-            res.json(Salas);
-        } else {
-            res.status(404).send({ message: 'Sala não encontrado' });
-        }
-    });
-};
-// Controlador para criar um novo Salas
-exports.createSalas = (req, res) => {
-    Salas.createSalas(req.body, (err, result) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.status(201).json(result);
-        }
-    });
-};
-// Controlador para atualizar um Salas existente
-exports.updateSalas = (req, res) => {
-    Salas.updateSalas(req.params.id, req.body, (err, result) => {
-        if (err) {
-            res.status(500).send(err);
-        } else if (result.changes) {
-            res.status(200).json(result);
-        } else {
-            res.status(404).send({ message: 'Sala não encontrado' });
-        }
-    });
-};
-// Controlador para deletar um Salas
-exports.deleteSalas = (req, res) => {
-    Salas.deleteSalas(req.params.id, (err, result) => {
-if (err) {
-        res.status(500).send(err);
-    } else if (result.changes) {
-        res.status(200).json({ message: 'Sala deletado com sucesso' });
-    } else {
-        res.status(404).send({ message: 'Sala não encontrado' });
-    }
     });
 };
