f117e0589c0dda503c71c288e92bfc042040eaa4
biaberganton <bibiaberganton@gmail.com>
2024-06-10 13:56:00 -0300

    atualizacaomodels

--- models/alocacoesModel.js
+++ models/alocacoesModel.js
@@ -19,46 +19,7 @@ function getAllAlocacao(callback) {
     });
 }
 
-// Função para buscar um cliente por ID
-function getAlocacaoById(id, callback) {
-    const db = openDbConnection();
-    db.get("SELECT * FROM Alocaçao WHERE id = ?", [id], (err, row) => {
-        db.close();
-        callback(err, row);
-    });
-}
-// Função para criar um novo cliente
-function createAlocacao(Alocacao, callback) {
-    const { id, idprof, idsala, periodo } = Alocacao;
-    const db = openDbConnection();
-    db.run("INSERT INTO Alocaçao (id, idprof,idsala, periodo) VALUES (?, ?, ?, ?)", [id,
-        idprof, idsala, periodo], function (err) {
-            db.close();
-            callback(err, { id: this.lastID });
-        });
-}
-// Função para atualizar um cliente existente
-function updateAlocacao(id, idprof, idsala, periodo) {
-    const { id, idprof, idsala, periodo } = Alocacao;
-    const db = openDbConnection();
-    db.run("UPDATE Alocaçao SET id = ?, idprof = ?, idsala = ?, periodo = ?  WHERE id = ?",
-        [id, idprof, idsala, periodo], function (err) {
-            db.close();
-            callback(err, { changes: this.changes });
-        });
-}
-// Função para deletar um cliente
-function deleteAlocacao(id, callback) {
-    const db = openDbConnection();
-    db.run("DELETE FROM Alocaçao WHERE id = ?", [id], function (err) {
-        db.close();
-        callback(err, { changes: this.changes });
-    });
-}
+
 module.exports = {
     getAllAlocacao,
-    getAlocacaoById,
-    createAlocacao,
-    updateAlocacao,
-    deleteAlocacao,
 };
