3b0f374edd259fddda04655fd70399abb7ebd242
GuDonizete <gustavolimasouza466@gmail.com>
2024-06-10 15:33:19 -0300

    contoller

--- controllers/alocacoesController.js
+++ controllers/alocacoesController.js
@@ -1,11 +1,13 @@
 const Alocacoes = require('../models/alocacoesModel');
 
-exports.getAllAlocacoes = (req, res) => {
-    Alocacoes.getAllAlocacoes((err, Alocacoes) => {
+exports.getAlocacoesById = (req, res) => {
+    Alocacoes.getAlocacoesById(req.params.id, (err, Alocacoes) => {
         if (err) {
             res.status(500).send(err);
-        } else {
+        } else if (Alocacoes) {
             res.json(Alocacoes);
+        } else {
+            res.status(404).send({ message: 'Alocacoes não encontrado' });
         }
     });
 };
